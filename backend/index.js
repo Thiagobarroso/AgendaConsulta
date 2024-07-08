d1240d2ad1b0a4ad1fec5f54b5a4231050793b7a
Thiago Barroso <thiagoo.silva@live.com>
2024-07-08 00:25:05 -0300

    adionando credenciais no login

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose'); // Importe o mongoose apenas uma vez
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 const routes = require('./src/routes/especialidade.routes');
+const usuarioRoutes = require('./src/routes/usuarios.routes');
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/agendaConsulta', {
@@ -14,10 +16,14 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
 
-    // Configuração do servidor após a conexão com o banco de dados
+    /// MIDDLEWARES
     app.use(bodyParser.json());
     app.use(morgan('dev'));
+    app.use(cors());
+
+    /// ROUTES
     app.use('/', routes);
+    app.use('/usuario', usuarioRoutes);
 
     app.listen(3000, () => {
       console.log('Servidor funcionando na porta 3000');
